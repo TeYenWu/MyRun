ceea426d521e904c7305a0d7d12c3018b8f61ea4
teyenwu <teyanwu@gmail.com>
2020-01-12 16:24:44 -0500

    fix_image_rotation_problem

--- app/src/main/java/com/teyenwu/myrun/MainActivity.java
+++ app/src/main/java/com/teyenwu/myrun/MainActivity.java
@@ -1,10 +1,13 @@
 package com.teyenwu.myrun;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -32,6 +35,8 @@ import com.soundcloud.android.crop.Crop;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
 import java.text.SimpleDateFormat;
@@ -40,7 +45,8 @@ import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
     private static final int  MY_PERMISSIONS_REQUEST_CAMERA = 0;
-    private static final int  MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 1;
+    private static final String URI_INSTANCE_STATE_KEY = "saved_uri";
+
 
     private static final int  REQUEST_IMAGE_CAPTURE = 3;
     private static final int  REQUEST_IMAGE_CROP = 4;
@@ -56,6 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Uri photoURI;
     String imageFilePath;
+    private boolean isTakenFromCamera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -86,14 +93,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         SharedPreferences local = this.getSharedPreferences("profile", 0);
 
         if(local.getBoolean("initialized", false)) {
-            String profileImageURI = local.getString("profileImageURI", "");
-            if (!profileImageURI.isEmpty()) {
-                photoURI = Uri.parse(profileImageURI);
-                setImageViewWithUri(profileImageView, photoURI);
-            }
+            loadSnap();
             selectedRadioButtonIndex = local.getInt("gender", 0);
             if(selectedRadioButtonIndex != -1){
                 ((RadioButton) genderRadioGroup.getChildAt(selectedRadioButtonIndex)).setChecked(true);
@@ -104,47 +108,27 @@ public class MainActivity extends AppCompatActivity {
             classEditText.setText(local.getString("class", ""));
             majorEditText.setText(local.getString("major", ""));
         }
-    }
 
-    private File createImageFile() throws IOException {
-        String timeStamp =
-                new SimpleDateFormat("yyyyMMdd_HHmmss",
-                        Locale.getDefault()).format(new Date());
-        String imageFileName = "IMG_" + timeStamp + "_";
-        File storageDir =
-                getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        imageFilePath = image.getAbsolutePath();
-        return image;
+        if (savedInstanceState != null) {
+            photoURI = savedInstanceState.getParcelable(URI_INSTANCE_STATE_KEY);
+        }
     }
 
     private void launchCamera(){
-        File photoFile = null;
-        try {
-            photoFile = createImageFile();
-        } catch (IOException ex) {
-            // Error occurred while creating the File
-            Toast.makeText(MainActivity.this, "Cannot access picture", Toast.LENGTH_SHORT).show();
-        }
-        if (photoFile != null) {
-//            this.grantUriPermission(packageName, uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                photoURI = FileProvider.getUriForFile(this,"com.teyenwu.fileprovider", photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                this.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            ContentValues values = new ContentValues(1);
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpg");
+            photoURI = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            takePictureIntent.putExtra("return-data", true);
+            this.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            isTakenFromCamera = true;
         }
     }
     public void changeProfile(){
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 this.requestPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_CAMERA);
                 //Create a file to store the image
@@ -167,26 +151,14 @@ public class MainActivity extends AppCompatActivity {
         editor.putInt("gender", selectedRadioButtonIndex);
         editor.putString("class", classEditText.getText().toString());
         editor.putString("major", majorEditText.getText().toString());
-        if(photoURI != null) {
-            editor.putString("profileImageURI", photoURI.toString());
-        }
         editor.apply();
+        saveSnap();
         finish();
     }
 
     public void cancel(View v) {
         // does something very interesting
         finish();
-    }
-
-    public void setImageViewWithUri(ImageView imageView, Uri uri){
-        Bitmap imageBitmap = null;
-        try {
-            imageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),uri);
-            imageView.setImageBitmap(imageBitmap);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
@@ -200,18 +172,35 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         return super.onOptionsItemSelected(item);
 
-
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        // Save the image capture uri before the activity goes into background
+        outState.putParcelable(URI_INSTANCE_STATE_KEY, photoURI);
+    }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-
-            Crop.of(photoURI, photoURI).asSquare().start(this);
+            Uri destination = Uri.fromFile(new File(getCacheDir(), "cropped"));
+            Crop.of(photoURI, destination).asSquare().start(this);
 
         } else if (requestCode == Crop.REQUEST_CROP  && resultCode == RESULT_OK) {
             if (data != null) {
-                setImageViewWithUri(profileImageView, photoURI);
+                Uri imageURI = Crop.getOutput(data);
+                profileImageView.setImageBitmap(null);
+                profileImageView.setImageURI(imageURI);
+            }
+
+            // Delete temporary image taken by camera after crop.
+            if (isTakenFromCamera) {
+                File f = new File(photoURI.getPath());
+                if (f.exists())
+                    f.delete();
+                isTakenFromCamera = false;
             }
         }
     }
@@ -221,19 +210,64 @@ public class MainActivity extends AppCompatActivity {
                                            String[] permissions, int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
                     launchCamera();
-                } else {
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(MainActivity.this, "Permission denied to use the camera", Toast.LENGTH_SHORT).show();
+                }else if (grantResults[0] == PackageManager.PERMISSION_DENIED || grantResults[1] == PackageManager.PERMISSION_DENIED){
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)||shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                            //Show an explanation to the user *asynchronously*
+                            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                            builder.setMessage("This permission is important for the app.")
+                                    .setTitle("Important permission required");
+                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 0);
+                                    }
+
+                                }
+                            });
+                            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, 0);
+                        }else{
+                            //Never ask again and handle your app without permission.
+                        }
+                    }
                 }
-                return;
             }
             // other 'case' lines to check for other
             // permissions this app might request.
+        }
+    }
+
+
+    private void loadSnap() {
+
+
+        // Load profile photo from internal storage
+        try {
+            FileInputStream fis = openFileInput(getString(R.string.profile_photo_file_name));
+            Bitmap bmap = BitmapFactory.decodeStream(fis);
+            profileImageView.setImageBitmap(bmap);
+            fis.close();
+        } catch (IOException e) {
+            // Default profile photo if no photo saved before.
+//            mImageView.setImageResource(R.drawable.default_profile);
+        }
+    }
+
+    private void saveSnap() {
+
+        // Commit all the changes into preference file
+        // Save profile image into internal storage.
+        profileImageView.buildDrawingCache();
+        Bitmap bmap = profileImageView.getDrawingCache();
+        try {
+            FileOutputStream fos = openFileOutput(getString(R.string.profile_photo_file_name), MODE_PRIVATE);
+            bmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
     }
 
